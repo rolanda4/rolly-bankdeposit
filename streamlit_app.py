d4ae91537d84dedad519905a524badb48ab229de
Rolanda Azeem <35231659+rolanda4@users.noreply.github.com>
2025-06-08 08:51:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from xgboost import XGBClassifier
+import joblib
 
